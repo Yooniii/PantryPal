2fdaa127776d11af068524f8fc281b6ad1e43e6d
Sarah Yoon <sarahyoon705@gmail.com>
2025-01-05 00:24:28 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+# import pages.toRecipe as toRecipe
+from openai import OpenAI
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+@app.route('/home.html', methods = ["POST", "GET"])
+def home():
+    return render_template('toRecipe.html')
+
+@app.route('/toRecipe', methods = ["GET", "POST"])
+def recipe():
+    try:     
+        print(f'### {request.full_path} : {request.method}')
+        if request.method == 'GET':
+            return render_template('toRecipe.html')
+
+        form_data = request.form
+        print(f'### form_data : {form_data}')
+        
+        # from toRecipe.html to these pages
+        if request.method == 'POST' and form_data.get('user-ingred'):
+            ingredients = form_data['user-ingred']
+            staples_list = form_data.getlist('staples') 
+            print(f'### STAPLES {type(staples_list)} : {staples_list}')
+            
+            staples = ""
+            staples = ", ".join(staples_list)
+            print(f'### list to String : {staples}')
+
+            if len(ingredients.strip()) == 0:
+                print('Not enough ingredients inputted')
+                return render_template('error.html')
+            
+            else: 
+                recipe_names, ingredients, images, = toRecipe.get_recipes(ingredients, staples)
+                return render_template('results.html', recipe_names=recipe_names, images=images)
+            
+        else:
+            return render_template('toRecipe.html')
+    
+    except Exception as e:
+        print('Error Occurred: ', e)
+        return render_template('error.html')
+
+# @app.route('/pantryTracker')
+# def ingredients():
+#    return render_template('pantryTracker.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=80, debug=True)
+
---
+++ pages/toRecipe.py
@@ -0,0 +1,53 @@
+toRecipe.py
+import os
+import requests
+import urllib.parse
+
+# Returns recipe names and missing ingredients
+def provide_recipe(ingredients, staples):
+    api_key = os.getenv('API_KEY')
+    all_ingred = ingredients + staples
+    URL = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={api_key}&ingredients={all_ingred}"
+    r = requests.get(url=URL)
+    data = r.json()
+
+    recipe_names = [recipe['title'] for recipe in data]
+    print(recipe_names)
+
+    missing_ingred_lists = [
+        [ingredient['name'] for ingredient in recipe['missedIngredients']]
+        for recipe in data
+    ]
+
+    print(missing_ingred_lists)
+    
+    return recipe_names, missing_ingred_lists
+
+
+# Returns array of image urls
+def provide_images(recipe_names):    
+    images = []
+    width=768
+    height=768
+    model='flux'
+    seed=None
+
+    for name in recipe_names:
+        prompt = f"Create a high-quality, visually stunning image of {name}, styled as a gourmet dish from a luxury restaurant."        
+        encoded_prompt = urllib.parse.quote(prompt)  # Encode prompt for URL
+        image_url = f"https://pollinations.ai/p/{encoded_prompt}?width={width}&height={height}&seed={seed}&model={model}"
+        images.append(image_url)
+        print(image_url)
+
+    return images
+        
+
+def get_recipes(ingredients, staples):
+    print('get_recipes starting...')
+    recipe_names, missing_ingredients = provide_recipe(ingredients, staples)
+    
+    print('provide_images processing...')
+    images = provide_images(recipe_names)
+    
+    return recipe_names, missing_ingredients, images
+
